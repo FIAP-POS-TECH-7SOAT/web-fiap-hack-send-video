8e012bc366393690e8c4ae4036d95f52f715b687
Thiago Rocha <tsrocha901@gmail.com>
2025-02-08 10:38:32 -0300

    fix auth and login

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,16 @@
 import { auth } from "@/auth";
 
 export default auth((req: any) => {
-  if (!req.auth && req.nextUrl.pathname !== "/user") {
-    const newUrl = new URL("/user", req.nextUrl.origin);
+  if (!req.auth && req.nextUrl.pathname !== "/user" && req.nextUrl.pathname !== "/login") {
+    const newUrl = new URL("/login", req.nextUrl.origin); // Redireciona para "/login"
     return Response.redirect(newUrl);
   }
 });
 
-export { auth as middleware,signOut } from "./auth";
+export { auth as middleware, signOut } from "./auth";
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|.*\\.svg$|user|api/auth/csrf).*)",
+    "/((?!api|_next/static|_next/image|.*\\.svg$|user|login|api/auth/csrf).*)",
   ],
 };
