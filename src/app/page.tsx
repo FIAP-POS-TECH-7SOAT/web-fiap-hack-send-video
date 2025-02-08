38e2aeeefe222f58a0a47a6413d0eb76cc8bfec5
Thiago Rocha <tsrocha901@gmail.com>
2025-02-07 21:36:45 -0300

    fix upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function UploadVideo() {
   const router = useRouter();
   const [videoFile, setVideoFile] = useState<any>(null);
   const [uploadProgress, setUploadProgress] = useState(0);
-  const [uploadId, setUploadId] = useState(null);
+  const [uploadId, setUploadId] = useState<string|null>(null);
   const { data: session } = useSession();
 
   const CHUNK_SIZE = 5 * 1024 * 1024; // 5MB
@@ -22,8 +22,10 @@ export default function UploadVideo() {
     setUploadProgress(0); // Reseta o progresso ao selecionar um novo arquivo
   };
 
-  const uploadChunk = async (file:any, partNumber:any, totalParts:any,fileName:string):Promise<{next_part:number}> => {
+  const uploadChunk = async (file:any, partNumber:any, totalParts:any,fileName:string,upload_id:string) => {
     try {
+      console.log('PARTESSSssss', partNumber);
+      
       if(!videoFile){
         
         
@@ -36,7 +38,7 @@ export default function UploadVideo() {
       formData.append("part_number", partNumber);
       formData.append("total_parts", totalParts);
       formData.append("file_name", fileName);
-      formData.append("upload_id", uploadId || '');
+      formData.append("upload_id", upload_id);
       
 
       
@@ -47,15 +49,15 @@ export default function UploadVideo() {
             Authorization: `Bearer ${session?.user?.token}`,
           },
         });
-    
+        setUploadProgress((partNumber / totalParts) * 100);
 
         if (!response.ok) {
           throw new ApplicationError(`Erro ao enviar file ${partNumber}`);
           
         }
         const json = await response.json()
-        setUploadId(json.upload_id);
-        return {next_part:json.next_part}
+        setUploadId(upload_id);
+       
       
     } catch (error) {
       
@@ -94,7 +96,7 @@ export default function UploadVideo() {
       const file = videoFile.slice(start, end);
 
       try {
-        await uploadChunk(file, i+1, totalParts,videoFile.name);
+        uploadChunk(file, i+1, totalParts,videoFile.name);
         setUploadProgress(((i + 1) / totalParts) * 100);
         if(i+1>=totalParts){
           ToastManager.success('Upload realizado com sucesso.',3000);
@@ -126,6 +128,85 @@ export default function UploadVideo() {
 
     
   };
+
+  const handleUpload2 = async () => {
+    try {
+     if (!videoFile) {
+       
+       throw new ApplicationError('Por favor, selecione um arquivo de vídeo.');
+       
+     }
+     const totalParts = Math.ceil(videoFile.size / CHUNK_SIZE);
+     
+ 
+     const responseGenerateKey = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/videos/generate-upload-key`,
+       {
+          method:"POST",
+          body:JSON.stringify({
+            file_name: videoFile.name,
+            total_parts:totalParts
+          }),
+         headers: {
+           Authorization: `Bearer ${session?.user?.token}`,
+           "Content-Type": "application/json"
+         },
+       }
+     )
+
+     if(!responseGenerateKey.ok){
+      throw new Error('Erro ao gerar upload id')
+     }
+     const jsonGenerate = await responseGenerateKey.json();
+
+     const response = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/videos/missing-parts?file_name=${videoFile.name}`,
+       {
+         headers: {
+           Authorization: `Bearer ${session?.user?.token}`,
+         },
+       }
+     )
+ 
+     const json = await response.json();
+ 
+     
+     
+     let missing_parts:number[] = json?.missing_parts && json?.missing_parts.length >0?json?.missing_parts : Array.from(
+      { length: totalParts },
+      (_, i) => i + 1,
+    );
+    
+
+    missing_parts.forEach((part,index)=>{
+      console.log('enviando parte',part);
+      
+      const start = index * CHUNK_SIZE;
+      const end = Math.min(start + CHUNK_SIZE, videoFile.size);
+      const file = videoFile.slice(start, end);
+
+      try {
+        uploadChunk(file, part, totalParts,videoFile.name,jsonGenerate.upload_id);
+        
+     
+      } catch(error) {
+        throw error
+        
+      }
+    })
+ 
+  
+    } catch (error) {
+     if(error instanceof ApplicationError){
+       
+       ToastManager.error(error.message,3000);
+     }else{
+ 
+       ToastManager.error('Erro ao enviar o arquivo. Tente novamente.',3000);
+     }
+    }
+ 
+     
+   };
+
 
   return (
     <div className="flex justify-center p-6">
@@ -163,7 +244,7 @@ export default function UploadVideo() {
             
           )}
           
-          <Button className="mt-2" onPress={handleUpload} color="primary" fullWidth isDisabled={Math.round(uploadProgress)>=100} isLoading={Math.round(uploadProgress)>0 && Math.round(uploadProgress)<100}>
+          <Button className="mt-2" onPress={handleUpload2} color="primary" fullWidth isDisabled={Math.round(uploadProgress)>=100} isLoading={Math.round(uploadProgress)>0 && Math.round(uploadProgress)<100}>
             Fazer Upload
           </Button>
         </CardBody>
