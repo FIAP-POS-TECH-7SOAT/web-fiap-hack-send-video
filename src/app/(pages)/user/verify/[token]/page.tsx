96e0d7a4802a4ea2afe2adea1003c6a0513ad0d3
Thiago Rocha <tsrocha901@gmail.com>
2025-02-08 10:23:47 -0300

    fix token page

--- src/app/(pages)/user/verify/[token]/page.tsx
+++ src/app/(pages)/user/verify/[token]/page.tsx
@@ -5,32 +5,13 @@ import React, {  useEffect, useState } from "react";
 
 
 import { useParams } from "next/navigation";
-import z from 'zod';
+
 import { CheckCircle, XCircle } from "lucide-react";
 import Link from "next/link";
 import LoadingScreen from "@/components/layout/loadingScreen";
 
 
 
-export function SuccessMessage() {
-  return (
-    <div className="flex items-center p-4 mb-4 text-green-800 bg-green-100 border border-green-300 rounded-lg">
-      <CheckCircle className="w-6 h-6 mr-2" />
-      <span>Sua conta foi ativada com sucesso!</span>
-      <Link className="ml-2 text-blue-800 underline" href={"/login"}> Ir para o login</Link>
-    </div>
-  );
-};
-
-export function ErrorMessage() {
-  return (
-    <div className="flex items-center p-4 mb-4 text-red-800 bg-red-100 border border-red-300 rounded-lg">
-      <XCircle className="w-6 h-6 mr-2" />
-      <span>Houve um erro ao ativar sua conta. Contate o suporte</span>
-      
-    </div>
-  );
-};
 
 
 export default function UserVerify() {
@@ -71,5 +52,13 @@ export default function UserVerify() {
     load()
   },[params.token])
 
-  return isLoading? <LoadingScreen/> : isError? <ErrorMessage/> :<SuccessMessage/>
+  return isLoading? <LoadingScreen/> : isError?  <div className="flex items-center p-4 mb-4 text-red-800 bg-red-100 border border-red-300 rounded-lg">
+  <XCircle className="w-6 h-6 mr-2" />
+  <span>Houve um erro ao ativar sua conta. Contate o suporte</span>
+  
+</div>: <div className="flex items-center p-4 mb-4 text-green-800 bg-green-100 border border-green-300 rounded-lg">
+      <CheckCircle className="w-6 h-6 mr-2" />
+      <span>Sua conta foi ativada com sucesso!</span>
+      <Link className="ml-2 text-blue-800 underline" href={"/login"}> Ir para o login</Link>
+    </div>
 }
